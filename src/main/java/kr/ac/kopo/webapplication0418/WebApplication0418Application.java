8af81524198105a7b9d9b5e3f3a608a13eb79ee9
kimprosminju <b.m1nj00371@gmail.com>
2024-04-18 09:22:18 +0900

    first commit

---
+++ src/main/java/kr/ac/kopo/webapplication0418/WebApplication0418Application.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.webapplication0418;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication0418Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication0418Application.class, args);
+    }
+
+}
