439a936e0feb931f6f75160c09b58f9ae3e51f31
KIMPROSMINJU <b.m1nj00371@gmail.com>
2024-08-27 15:09:35 +0900

    first commit

--- src/main/java/kr/ac/kopo/board/entity/BaseEntity.java
+++ src/main/java/kr/ac/kopo/board/entity/BaseEntity.java
@@ -1,8 +1,11 @@
 package kr.ac.kopo.board.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -11,6 +14,12 @@ import java.time.LocalDateTime;
 @EntityListeners(value = {AuditingEntityListener.class})
 @Getter
 abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "regDate", updatable = false)
     private LocalDateTime regDate; // 등록 날짜, 시간
+
+    @LastModifiedDate
+    @Column(name = "modDate")
     private LocalDateTime modDate; // 수정 날짜, 시간
 }
