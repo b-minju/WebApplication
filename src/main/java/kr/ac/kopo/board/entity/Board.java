abe8df86f21c195b87193a1bb44920060ad18cbb
KIMPROSMINJU <b.m1nj00371@gmail.com>
2024-09-03 13:57:03 +0900

    first commit

--- src/main/java/kr/ac/kopo/board/entity/Board.java
+++ src/main/java/kr/ac/kopo/board/entity/Board.java
@@ -1,9 +1,6 @@
 package kr.ac.kopo.board.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -11,11 +8,14 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-@ToString
+@ToString(exclude = "writer")
 public class Board extends BaseEntity{
     @Id // 기본 키 설정
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 1씩 자동 증가 auto-increment
     private Long bno;
     private String title;
     private String content;
+
+    @ManyToOne
+    private Member writer; // foreign key 설정 - 참조 무결성
 }
