4d8d70d7018605ff817928c7411576dd93c38a7c
KIMPROSMINJU <b.m1nj00371@gmail.com>
2024-09-24 14:09:03 +0900

    last class

---
+++ src/test/java/kr/ac/kopo/board/service/BoardServiceTests.java
@@ -0,0 +1,23 @@
+package kr.ac.kopo.board.service;
+
+import kr.ac.kopo.board.DTO.BoardDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BoardServiceTests {
+    @Autowired
+    private BoardService boardService;
+
+    @Test
+    public void testRegister() {
+        BoardDTO dto = BoardDTO.builder()
+                .title("Test.")
+                .content("Test...")
+                .writerEmail("user55@aaa.com")
+                .build();
+
+        Long bno = boardService.register(dto);
+    }
+}
