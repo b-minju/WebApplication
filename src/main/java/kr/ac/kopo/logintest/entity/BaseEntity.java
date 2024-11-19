ab1ccb33a67462152ed56c3dd92c88f83a70f0c5
KIMPROSMINJU <b.m1nj00371@gmail.com>
2024-11-19 16:16:20 +0900

    1119 class

---
+++ src/main/java/kr/ac/kopo/logintest/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package kr.ac.kopo.logintest.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(value={AuditingEntityListener.class})
+@Getter
+abstract class BaseEntity {
+    @CreatedDate
+    @Column(name="regDate")
+    private LocalDateTime regDate; // 등록날짜
+
+    @LastModifiedDate
+    @Column(name="modDate")
+    private LocalDateTime modDate; // 수정날짜
+}
